087d285cf9b0621b558557b6843ac820fd64a89b
congphuong1606 <cphuong.coldboy@gmail.com>
2018-10-25 12:50:04 +0700

    created fordertoken.io

---
+++ private_html/filemanager/config/.htusers.php
@@ -0,0 +1,6 @@
+<?php
+        if( !defined( '_JEXEC' ) && !defined( '_VALID_MOS' ) ) die( 'Restricted access' );
+        $GLOBALS["users"]=array(
+        array('admin','635d8890b72887186f245f634fd4c4d4','/home','http://localhost','1','','7',1),
+);
+?>
